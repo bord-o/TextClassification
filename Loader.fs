aac3638c7feb10d062d928a3511422ff5092b7a4
blittle <blittle@uphp.com>
2024-01-17 13:34:27 -0500

    start csv processing

--- Loader.fs
+++ Loader.fs
@@ -2,16 +2,24 @@
 
 open System.IO
 open System.Text
-
+open FSharp.Data
 module Loader =
 
 
-    let initData (dataDir:string) =
+    //let datadir = @"C:\Users\brody\source\repos\TextClassification\Data\"
+    let dataDir = @"C:\Users\BLittle\Documents\GIT\TextClassification\Data\"
+    [<Literal>]
+    let fulldata = @"C:\Users\BLittle\Documents\GIT\TextClassification\Data\test.csv"
+    
+
+    type DataRow = CsvProvider<fulldata>
+
+    let initData () =
 
         let test = File.ReadAllBytes(Path.Join(dataDir, "test.csv" )) 
         let train = File.ReadAllBytes(Path.Join(dataDir, "train.csv")) 
         let sample = test[0..10]
-        ()
+        (sample, test)
 
         //printfn $"{test}"
         //printfn $"{train}"
--- Program.fs
+++ Program.fs
@@ -1,23 +1,23 @@
 ﻿namespace TextClassification
 open System
+open System.Text
 // Usage:
 module Main = 
-    let original = Mydata.Lorem.lorem
-    let compressed = Gzip.compress original
-    let decompressed = Gzip.decompress compressed
-    //printfn "Original: %s" original
-    let datadir = @"C:\Users\brody\source\repos\TextClassification\Data\"
+    let (train, test)  = Loader.initData ()
 
-    [<EntryPoint>]
-    let main args =
+    let compressionTest data =  
+        let original = Encoding.UTF8.GetString test
+        let compressed = Gzip.compress original
+        let decompressed = Gzip.decompress compressed
         printfn "Original Length: %A" original.Length
         //printfn "Compressed: %A" compressed.Length
         printfn "Compressed Length: %A" compressed.Length
         //printfn "Decompressed: %s" decompressed
         printfn "Decompressed Length: %A" decompressed.Length
-        Loader.initData datadir
-        printfn "Done Reading"
-        let _ = Console.ReadLine ()
+        Console.ReadLine ()
+
+    [<EntryPoint>]
+    let main args =
         0
         
 
