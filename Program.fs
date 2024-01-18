fb58deb298f7c453ce247b88abf57ee12eff9313
Brody <brodylittle011@gmail.com>
2024-01-17 23:26:48 -0500

    save to file

--- Program.fs
+++ Program.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Text
+open System.IO
 // Usage:
 module Main =
     let (train, test, sample) = Loader.initData ()
@@ -21,5 +22,7 @@ module Main =
     let main args =
         // Seq.iter (fun row -> printfn "%A" row) sample
         // Knn.predict "The news was wrong" train 10
-        let accuracy = Knn.test (Seq.take 1000 test) train 3
+        let accuracy = Knn.test (Seq.take 100 test) train 3
+        File.WriteAllText("./results.txt", $"The overall accuracy was: {accuracy}")
+        
         int (accuracy * 100.0)
